36474acdac41d15e562edc0891a4b54fecb3dfbd
wasuken <wevorence@gmail.com>
2018-09-19 19:30:28 +0900

    added niconico watch

--- Gemfile
+++ Gemfile
@@ -8,3 +8,5 @@ git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
 gem "sinatra"
 gem "nokogiri"
 gem "parseconfig"
+gem 'todoist-ruby'
+gem 'julius'
--- jsay.rb
+++ jsay.rb
@@ -1,6 +1,13 @@
 # coding: utf-8
 require "sinatra"
 require "rss"
+require "todoist"
+require "nokogiri"
+require "parseconfig"
+require "open-uri"
+
+CONFIG=ParseConfig.new("config")
+CLIENT = Todoist::Client.create_client_by_token(CONFIG["token"])
 
 get '/' do
   say=params[:say]||"test"
@@ -12,6 +19,23 @@ get '/' do
     now_weather
   when "time" then
     now_time
+  when "todo" then
+    items_list =  CLIENT.sync_items.collection
+    p items_list.first.first
+    items_list.each do |i|
+      jsay(i.last.content)
+    end
+  when "watchnico" then
+    # ニコニコ生放送から今配信している放送のタイトルを取得する。
+    url="http://live.nicovideo.jp/search?keyword=%E3%83%97%E3%83%AD%E3%82%B0%E3%83%A9%E3%83%9F%E3%83%B3%E3%82%B0&sort=recent"
+    html = open(url) do |f|
+      charset = f.charset
+      f.read
+    end
+    doc=Nokogiri::HTML.parse(html)
+    doc.css(".result-list").first.css("a").each do |node|
+      jsay(node.text())
+    end
   end
 end
 def get_rss_items()
